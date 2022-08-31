d679deaf3c8bc17726baeaacc9334d61dc09ab88
The Exorcist <whitebeard086@gmail.com>
2022-08-31 22:30:11 +0100

    Feed & Sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,38 @@
-const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
-export default Feed
+import { Box, Stack, Typography } from "@mui/material";
+import { useState, useEffect } from "react";
+import { Sidebar, Videos } from ".";
+
+const Feed = () => (
+  <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+    <Box
+      sx={{
+        height: { sx: "auto", md: "92vh" },
+        borderRight: "1px solid #3d3d3d",
+        px: { sx: 0, md: 2 },
+      }}
+    >
+      <Sidebar />
+      <Typography
+        className="copyright"
+        variant="body2"
+        sx={{ mt: 1.5, color: "#fff" }}
+      >
+        Copyright 2022 Alex
+      </Typography>
+    </Box>
+    <Box
+      p={2}
+      sx={{
+        overflowY: "auto",
+        height: "90vh",
+        flex: 2,
+      }}
+    >
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+        New <span style={{ color: "#F31503" }}>videos</span>
+      </Typography>
+      <Videos videos={[]} />
+    </Box>
+  </Stack>
+);
+export default Feed;
