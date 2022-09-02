9e47f1ba83326eb2b84f35c68101e260fe7c7d7e
The Exorcist <whitebeard086@gmail.com>
2022-09-02 18:01:43 +0100

    Videos

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -5,7 +5,7 @@ import { Sidebar, Videos } from ".";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const Feed = () => {
-  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [selectedCategory, setSelectedCategory] = useState("JS Mastery");
   const [videos, setVideos] = useState([]);
 
   useEffect(() => {
