f5d64e7931d140da2d9b84bdfe9bc0a6758803f3
The Exorcist <whitebeard086@gmail.com>
2022-09-03 07:56:05 +0100

    Channel detail page

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,6 +1,46 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import { Videos, ChannelCard } from ".";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
 const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+
+  const { id } = useParams();
+
+  console.log(channelDetail, videos);
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) =>
+      setChannelDetail(data?.items[0])
+    );
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then(
+      (data) => setVideos(data?.items)
+    );
+  }, [id]);
+
   return (
-    <div>ChannelDetail</div>
-  )
-}
-export default ChannelDetail
+    <Box minHeight="95vh">
+      <Box>
+        <div
+          style={{
+            background:
+              "linear-gradient(90deg, rgba(120,36,107,1) 0%, rgba(63,63,98,1) 41%, rgba(20,68,51,1) 100%)",
+            zIndex: 10,
+            height: "300px",
+          }}
+        />
+        <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
+      </Box>
+      <Box display="flex" p="2">
+        <Box sx={{ mr: { sm: "100px" } }} />
+        <Videos videos={videos} />
+      </Box>
+    </Box>
+  );
+};
+export default ChannelDetail;
